c2b93d87d8dfc691a7822dcb7bc9f57fbdd28ce4
John Wagner <wajo432@gmail.com>
2024-12-02 22:00:20 -0600

    Day02 recovered from my brain

--- Day02/bin/main.ml
+++ Day02/bin/main.ml
@@ -14,9 +14,27 @@ let read_lines file =
 let parse_input (lines: string list): (int list) list = lines
 |> List.map (List.map int_of_string % String.split_on_char ' ')
 
-let part1 = read_lines "input.txt" |> parse_input |> List.length
+let rec is_valid asc = function
+  | [] -> true
+  | [_] -> true
+  | (x::y::xs) -> let d = abs (x - y) in
+    d >= 1 && d <= 3 && asc = (x < y) && is_valid asc (y::xs)
 
-let part2 = 0
+let part1 = read_lines "input.txt"
+  |> parse_input
+  |> List.filter (fun seq -> is_valid true seq || is_valid false seq)
+  |> List.length
+
+let alternatives xs = (0 -- (List.length xs - 1))
+  |> Enum.map (fun i -> List.filteri (fun n _ -> n <> i) xs)
+  |> List.of_enum
+
+let part2 = read_lines "input.txt"
+  |> parse_input
+  |> List.filter (fun seq ->
+      let possibilities = (seq::alternatives seq) in
+      List.exists (fun l -> is_valid true l || is_valid false l) possibilities)
+  |> List.length
 
 let () =
   Printf.printf "Part 1: %i\n" part1;
